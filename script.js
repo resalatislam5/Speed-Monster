29b33f941f54730bf8e0dce9f25174d538c5d9f6
resalatislam5 <resalatislam5@gmail.com>
2022-09-15 00:16:07 +0600

    srtInterval problem fixied

--- script.js
+++ script.js
@@ -132,7 +132,5 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
-
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
